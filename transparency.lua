b46873e3e5deb413e4cfb222dc0876eccd2447b6
Terry <terry.h@outlook.com>
2015-12-29 01:48:10 -0800

    Reduced code replication

--- transparency.lua
+++ transparency.lua
@@ -1,7 +1,15 @@
 -- Script to handle transparency through scrolling using transset-df
 
 function inc_opacity()
-	local handle = io.popen("transset-df --inc -p .03")
+	change_opacity("transset-df --inc -p .03")
+end
+
+function dec_opacity()
+	change_opacity("transset-df --dec -p .03")
+end
+
+function change_opacity(command)
+	local handle = io.popen(command)
 	local result = handle:read("*a")
 	handle:close()
 
@@ -12,23 +20,5 @@ function inc_opacity()
 	mp.osd_message("Opacity: " .. opacity .. "%")
 end
 
-function dec_opacity()
-	local handle = io.popen("transset-df --dec -p .03")
-	local result = handle:read("*a")
-	handle:close()
-
-	local opacity = string.gsub(result, "Set Property to ", "")
-	opacity = tonumber(opacity)
-	opacity = opacity * 100
-
-	mp.osd_message("Opacity: ".. opacity .. "%")
-end
-
-function toggle_opacity()
-	os.execute("transset-df -t -p")
-	mp.osd_message("Toggled Opacity")
-end
-
 mp.add_key_binding("MOUSE_BTN3", "inc_opacity", function() inc_opacity() end)
 mp.add_key_binding("MOUSE_BTN4", "dec_opacity", function() dec_opacity() end)
-mp.add_key_binding("MOUSE_BTN1", "toggle_opacity", function() toggle_opacity() end)
